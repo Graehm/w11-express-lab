97704e904c55e42b8e441f37888f94569f9579b7
Graehm <gpatrellfazio@gmail.com>
2023-06-05 22:00:44 -0500

    pt4 delete

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 //containing all the functionality 
+require('dotenv').config()
 const User = require('../models/user')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
@@ -17,9 +18,9 @@ exports.auth = async (req, res, next) => {
         res.status(401).send('Not authorized')
     }
 }
-
+// this line stats a try block and incating that the code inside the blick will be executed and 
 exports.createUser = async (req, res) => {
-    try{
+    try{    
         const user = new User(req.body)
         await user.save()
         const token = await user.generateAuthToken()
@@ -28,3 +29,49 @@ exports.createUser = async (req, res) => {
         res.status(400).json({message: error.message})
       } 
     }
+// 2. 
+exports.loginUser = async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email })
+        if (!user || !await bcrypt.compare(req.body.password, user.password)) {
+            res.status(400).send(('Invalid Login Attempt'))
+        } else {
+            const token = await user.generateAuthToken()
+            res.json({ user, token })
+        }
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
+}
+
+exports.updateUser = async (req, res) => {
+    try{
+      const updates = Object.keys(req.body) //this line retrieves the keys of the req.bpdy object and assings them to the updates variables 
+      const user = await User.findOne({ _id: req.params.id }) //find the user in the database withthe given id
+      updates.forEach(update => user[update] = req.body[update]) // iterate over the updates array to see what the prop
+      await user.save()  //
+      res.json(user)
+    }catch(error){
+      res.status(400).json({message: error.message})
+    }
+    
+  }
+
+  exports.deleteUser = async (req, res) => {
+    try{
+      await req.user.deleteOne()
+      res.json({ message: 'User deleted' })
+    }catch(error){
+      res.status(400).json({message: error.message})
+    }
+  }
+
+exports.logoutUser = async (req, res) => {
+    try {
+        // const user = await User.findOne({ email: req.body.email})
+        console.log('a string')
+        res.send('logged in bud')
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,8 +3,9 @@ const router = express.Router()// just the router part becuase we want to isolat
 const userController = require('../controllers/userController')
 
 router.post('/', userController.createUser)
-// router.post('/login', userController.loginUser)
-// router.put('/:id', userController.updateUser)
-// router.delete('/:id', userController.auth, userController.deleteUser)
+router.post('/login', userController.loginUser)
+router.put('/:id', userController.updateUser)
+router.delete('/:id', userController.auth, userController.deleteUser)
+router.post('/logout', userController.auth, userController.logoutUser)
 
 module.exports = router
